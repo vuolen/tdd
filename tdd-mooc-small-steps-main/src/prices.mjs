c38ae83ca80b11e383c14955ddc46e0cee120bd8
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-30 16:40:33 +0200

    step 9

--- tdd-mooc-small-steps-main/src/prices.mjs
+++ tdd-mooc-small-steps-main/src/prices.mjs
@@ -60,6 +60,7 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       const newDate = Temporal.PlainDate.from(row.holiday)
+      holiday.year = newDate.year
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&

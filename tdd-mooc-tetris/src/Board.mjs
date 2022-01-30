0ed0a2ad03b24aee4661d943ce1493994ea5c4a6
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-30 18:06:18 +0200

    board is now two dimensional

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -3,20 +3,26 @@ import { Block } from "./Block.mjs";
 export class Board {
   width;
   height;
-  column;
+  columns;
   fallingPiece;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.pieceFalling = false;
-    this.column = Array(height).fill(Block.EMPTY)
+    this.columns = []
+    for (let i = 0; i < this.width; i++) {
+      this.columns.push(Array(height).fill(Block.EMPTY))
+    }
   }
 
   toString() {
     let str = ""
-    for (let i = 0; i < this.height; i++) {
-      str += `.${this.column[i].color}.\n`
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        str += this.columns[col][row].color
+      }
+      str += "\n"
     }
     return str
   }
@@ -25,16 +31,20 @@ export class Board {
     if (this.fallingPiece) {
       throw new Error("already falling")
     }
-    this.column[0] = block
+    this.columns[Math.floor(this.width / 2)][0] = block
     this.fallingPiece = block
   }
 
   tick() {
+    this.columns.forEach(column => this.tickColumn(column))
+  }
+
+  tickColumn(column) {
     for (let i = this.height - 1; i > 0; i--) {
-      if (this.column[i] === Block.EMPTY) {
-        this.column[i] = this.column[i - 1]
-        this.column[i - 1] = Block.EMPTY
-      } else if (this.column[i] === this.fallingPiece) {
+      if (column[i] === Block.EMPTY) {
+        column[i] = column[i - 1]
+        column[i - 1] = Block.EMPTY
+      } else if (column[i] === this.fallingPiece) {
         this.fallingPiece = undefined
       }
     }

88848c45e57472a65b005066f431bb98410aa708
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-30 20:49:39 +0200

    Squashed commit of the following:
    commit 16a54fb009675516cbca6f520b7aaf05db32aec2
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 20:45:48 2022 +0200
    
        third test
    
    commit c1b6f287ecb6ca6e1cb2bc3f771450f4dc38b6dd
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 20:45:15 2022 +0200
    
        morerefactoring
    
    commit a61ff2ce4c03d9c0ac050686a070b6e2c464b5a8
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 20:30:44 2022 +0200
    
        refactoring
    
    commit 903de01d82f089ad0991815b14185a7a2b04cf84
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 18:42:51 2022 +0200
    
        second test
    
    commit b9426e50b4053ae3fbaadd910d6f456d967b7170
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 18:23:58 2022 +0200
    
        first fallingtetrominoes test
    
    commit 0ed0a2ad03b24aee4661d943ce1493994ea5c4a6
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 18:06:18 2022 +0200
    
        board is now two dimensional
    
    commit 22c92a1e6b6ecdb828c0871378360e8ce5677113
    Author: Lennu Vuolanne <vuolanne.lennu@gmail.com>
    Date:   Sun Jan 30 17:21:20 2022 +0200
    
        board now uses blocks

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,39 +1,87 @@
+import { Block } from "./Block.mjs";
+import { Tetromino } from "./Tetromino.mjs";
+
 export class Board {
   width;
   height;
-  column;
+  columns;
   fallingPiece;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.pieceFalling = false;
-    this.column = Array(height).fill(".")
+    this.columns = []
+    for (let i = 0; i < this.width; i++) {
+      this.columns.push(Array(height).fill(Block.EMPTY))
+    }
   }
 
   toString() {
     let str = ""
-    for (let i = 0; i < this.height; i++) {
-      str += `.${this.column[i]}.\n`
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        str += this.columns[col][row].color
+      }
+      str += "\n"
     }
     return str
   }
 
-  drop(block) {
+  drop(piece) {
     if (this.fallingPiece) {
       throw new Error("already falling")
     }
-    this.column[0] = block.color
-    this.fallingPiece = block.color
+    if (piece instanceof Block) {
+      this.columns[Math.floor(this.width / 2)][0] = piece
+      this.fallingPiece = [piece]
+    } else if (piece instanceof Tetromino) {
+      const shape = piece.getShape()
+      const blocks = shape.shape.map(char => char === "." ? Block.EMPTY : new Block(char))
+      const leftMargin = Math.floor(this.width / 2 - shape.size / 2)
+      this.fallingPiece = []
+      for (let i = 0; i < shape.size; i++) {
+        for (let j = 0; j < shape.size; j++) {
+          const block = blocks[i + j * shape.size]
+          if (block !== Block.EMPTY) {
+            this.columns[leftMargin + i][j] = block
+            this.fallingPiece.push(block)
+          }
+        }
+      }
+    }
   }
 
   tick() {
+    if (this.fallingPiece !== undefined && this.columns.every(column => this.canMove(column))) {
+      this.columns.forEach(column => this.tickColumn(column))
+    } else {
+      this.fallingPiece = undefined;
+    }
+  }
+
+  canMove(column) {
+    for (let i = this.height - 1; i >= 0; i--) {
+      if (this.fallingPiece.includes(column[i])) {
+        if (i + 1 === this.height) {
+          return false;
+        }
+        const nextBlock = column[i + 1];
+        if (nextBlock === Block.EMPTY || this.fallingPiece.includes(nextBlock)) {
+          continue;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  tickColumn(column) {
     for (let i = this.height - 1; i > 0; i--) {
-      if (this.column[i] === ".") {
-        this.column[i] = this.column[i - 1]
-        this.column[i - 1] = "."
-      } else if (this.column[i] === this.fallingPiece) {
-        this.fallingPiece = undefined
+      if (this.fallingPiece.includes(column[i - 1])) {
+        column[i] = column[i - 1]
+        column[i - 1] = Block.EMPTY
       }
     }
   }
--- tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
@@ -1,4 +1,3 @@
-/*
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
@@ -28,7 +27,7 @@ describe("Falling tetrominoes", () => {
     );
   });
 
-  xit("stop when they hit the bottom", () => {
+  it("stop when they hit the bottom", () => {
     board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
 
@@ -42,7 +41,7 @@ describe("Falling tetrominoes", () => {
     );
   });
 
-  xit("stop when they land on another block", () => {
+  it("stop when they land on another block", () => {
     board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
     board.drop(Tetromino.T_SHAPE);
@@ -58,4 +57,3 @@ describe("Falling tetrominoes", () => {
     );
   });
 });
-*/

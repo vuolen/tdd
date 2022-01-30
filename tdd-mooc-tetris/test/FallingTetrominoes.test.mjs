16a54fb009675516cbca6f520b7aaf05db32aec2
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-30 20:45:48 +0200

    third test

--- tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
@@ -41,7 +41,7 @@ describe("Falling tetrominoes", () => {
     );
   });
 
-  xit("stop when they land on another block", () => {
+  it("stop when they land on another block", () => {
     board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
     board.drop(Tetromino.T_SHAPE);
